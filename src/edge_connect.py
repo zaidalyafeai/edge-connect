e7e08e0c5e8fb9a5352f0d05950a6cef62514598
Zaid Alyafeai <alyafey22@gmail.com>
2019-06-16 23:59:09 +0300

    Update edge_connect.py

--- src/edge_connect.py
+++ src/edge_connect.py
@@ -100,7 +100,7 @@ class EdgeConnect():
             elif model == 2:
                 outputs = self.inpaint_model(images, edges, masks)
                 #outputs_merged = (outputs * masks) + (images * (1 - masks))
-                outputs_merged = outputs + images
+                outputs_merged = outputs
 
             # inpaint with edge model / joint model
             else:
